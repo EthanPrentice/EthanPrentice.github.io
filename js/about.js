a292647a26f2212533c269ee633e0d35b93b9ae1
Ethan Prentice <ethanp5262@gmail.com>
2018-03-04 18:36:08 -0500

    Updated Style

--- js/about.js
+++
@@ -1,6 +0,0 @@
-function init() {
-  $ ("#title_container").css({opacity: '0', top: '80%'});
-  $ ("#title_container").animate({opacity: '1', top: '50%'}, 1200);
-}
-
-$( window ).on( "load", init);
--- js/main.js
+++ js/main.js
@@ -1,7 +1,50 @@
-function init() {
-  $ ("#title_container").css({opacity: '0', top: '80%'});
-  $ ("#title_container").animate({opacity: '1', top: '50%'}, 1200);
-  $ (".box").animate({opacity: '1'}, 1500);
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
 }
 
-$( window ).on( "load", init);
+function moveArrow() {
+  $ ("#dwn_arrow").animate({top: "93%", opacity: "0.85"}, 1000, function() {
+    $ ("#dwn_arrow").animate({top: "91%", opacity: "1"}, 1000, moveArrow);
+  });
+}
+
+
+function init() {
+  setTimeout(moveArrow, 2000);
+  var displacement= $ ('#carleton_logo').css.width;
+  $('#about_me_p').css.transform = 'translateX(-' + displacement + ')';
+}
+
+function checkVisibilities() {
+  
+  if ( $('#about_me_heading').visible(false) ) {
+    $('#about_me_heading').animate({fontSize: "45px", opacity: "1"}, 900);
+    $('.current').removeClass("current");
+    $("#about_btn").addClass("current");
+  }
+  
+  if (!($("#main_section").visible(true))) {
+    if (!$("#main_container").visible(true)) {
+      $("#about_me_section").css({marginTop: "65px"});
+      $("header").css({position: "fixed", top: "0"});
+    }
+  } else {
+    $("#about_me_section").css({marginTop: ""});
+    $("header").css({position: "", top: ""});
+  }
+  
+}
+
+$(window).scroll(checkVisibilities);
+
+
+$( window ).on("load", init);
