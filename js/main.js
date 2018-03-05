a21ac96f800aa675ff8abcb87ad6acc1656013f5
Ethan Prentice <ethanp5262@gmail.com>
2018-03-04 23:12:22 -0500

    Added information to about section

--- js/main.js
+++ js/main.js
@@ -21,28 +21,38 @@ function moveArrow() {
 function init() {
   setTimeout(moveArrow, 2000);
   var displacement= $ ('#carleton_logo').css.width;
-  $('#about_me_p').css.transform = 'translateX(-' + displacement + ')';
+  $('#about_p').css.transform = 'translateX(-' + displacement + ')';
 }
 
 function checkVisibilities() {
   
-  if ( $('#about_me_heading').visible(false) ) {
-    $('#about_me_heading').animate({fontSize: "45px", opacity: "1"}, 900);
+  // AboutMe Animations
+  if ( $('#about_heading').visible(false) ) {
+    $('#about_heading').animate({fontSize: "45px", opacity: "1"}, 900);
     $('.current').removeClass("current");
     $("#about_btn").addClass("current");
   }
   
+  if ($("#carleton_logo").visible(true)) {
+    // Correct width so not on a half pixel
+    var desiredWidth = $("#carleton_logo").parent().width()*0.1 - ($("#carleton_logo").parent().width()*0.1) % 2;
+    $("#carleton_logo").animate({width: desiredWidth, opacity: "1"}, 1200);
+  }
+  
+  
+  // Header Transitions
   if (!($("#main_section").visible(true))) {
     if (!$("#main_container").visible(true)) {
-      $("#about_me_section").css({marginTop: "65px"});
+      $("#about_section").css({marginTop: "65px"});
       $("header").css({position: "fixed", top: "0"});
     }
   } else {
-    $("#about_me_section").css({marginTop: ""});
+    $("#about_section").css({marginTop: ""});
     $("header").css({position: "", top: ""});
   }
   
 }
+
 
 $(window).scroll(checkVisibilities);
 
