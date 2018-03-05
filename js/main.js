2034837ef01b9680563b036e1731b4b760144386
Ethan Prentice <ethanp5262@gmail.com>
2018-03-05 15:52:57 -0500

    Navbar improvements
    known bugs: delay on highlighting navbar selection on navbar button press

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-function fade(element) {
-    var op = 1;  // initial opacity
-    var timer = setInterval(function () {
-        if (op <= 0.1){
-            clearInterval(timer);
-            element.style.display = 'none';
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op -= op * 0.1;
-    }, 50);
-}
-
 function moveArrow() {
   $ ("#dwn_arrow").animate({top: "93%", opacity: "0.85"}, 1000, function() {
     $ ("#dwn_arrow").animate({top: "91%", opacity: "1"}, 1000, moveArrow);
@@ -18,25 +5,77 @@ function moveArrow() {
 }
 
 
-function init() {
-  setTimeout(moveArrow, 2000);
-  var displacement= $ ('#carleton_logo').css.width;
-  $('#about_p').css.transform = 'translateX(-' + displacement + ')';
+function changeCurrentNavBar(elementID) {
+  console.log( elementID );
+  $('.current').animate({color: '#e1e1e1'}, 300, function() {
+    
+    $('.current').removeClass("current");
+        
+    $( elementID ).addClass("current");
+    
+    $('.current').animate({color: '#1f8f80'}, 300);
+  });
 }
 
+
+function init() {
+  setTimeout(moveArrow, 2000);
+  
+  current_section = $( "#main_section" );
+  
+  var displacement= $ ('#carleton_logo').css.width;
+  $('#about_p').css.transform = 'translateX(-' + displacement + ')';
+  
+  $ ('navbar li').click(function() {
+    navBarBtnPress($(this).attr('id'));
+  });
+}
+
+
 function checkVisibilities() {
+
+  $("section").each(function() {
+    
+      if (this !== current_section) {
+          
+        if ( this.getBoundingClientRect().top < $(window).height()/2) {      
+          if ( this.getBoundingClientRect().bottom > $(window).height()/2) {
+
+            console.log("Switching to: " + $(this).attr('id'));
+            
+            current_section = this;
+
+            switch ($(this).attr('id')) { 
+              case "main_section":
+                changeCurrentNavBar("#home_btn");
+                break;
+              case "about_section":
+                changeCurrentNavBar("#about_btn");
+                break;
+              case "projects_section":
+                changeCurrentNavBar("#projects_btn");
+                break;
+            }
+
+          }
+        }
+      }
+    });
   
   // AboutMe Animations
   if ( $('#about_heading').visible(false) ) {
     $('#about_heading').animate({fontSize: "45px", opacity: "1"}, 900);
-    $('.current').removeClass("current");
-    $("#about_btn").addClass("current");
   }
   
   if ($("#carleton_logo").visible(true)) {
     // Correct width so not on a half pixel
     var desiredWidth = $("#carleton_logo").parent().width()*0.1 - ($("#carleton_logo").parent().width()*0.1) % 2;
     $("#carleton_logo").animate({width: desiredWidth, opacity: "1"}, 1200);
+  }
+  
+  // Projects Animations
+  if ( $('#projects_heading').visible(false) ) {
+    $('#projects_heading').animate({fontSize: "45px", opacity: "1"}, 900);
   }
   
   
@@ -53,6 +92,44 @@ function checkVisibilities() {
   
 }
 
+
+function navBarBtnPress(elementID) {
+  var pos;
+  var headerHeight = $( "#header" ).height();
+  console.log($( "navbar" ).height());
+  switch (elementID) {
+    case "home_btn":
+      pos = $( "#main_section" ).position();
+      $('html, body').animate({
+        scrollTop: 0
+      }, 400);
+      changeCurrentNavBar("home_btn");
+      current_section = $( "home_section" );
+      break;
+    
+    case "about_btn":
+      pos = $( "#about_section" ).position();
+      $('html, body').animate({
+        scrollTop: $( "#about_section").offset().top - headerHeight
+      }, 400);
+      changeCurrentNavBar("about_btn");
+      current_section = $( "about_section" );
+      break;
+      
+    case "projects_btn":
+      pos = $( "#projects_section" ).position();
+      $('html, body').animate({
+        scrollTop: $( "#projects_section").offset().top - headerHeight
+      }, 400);
+      changeCurrentNavBar("projects_btn");
+      current_section = $( "projects_section" );
+      break;
+  }
+}
+
+
+
+var current_section;
 
 $(window).scroll(checkVisibilities);
 
